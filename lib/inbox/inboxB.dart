b669611521d0d66271f3021c0e2ef42d2409ad6b
abhishekkumar <abhishek.kp22@gmail.com>
2021-05-21 12:59:20 +0530

    updated1

--- lib/inbox/inboxB.dart
+++ lib/inbox/inboxB.dart
@@ -31,52 +31,482 @@ class _InboxBState extends State<InboxB> {
 
       body: Container(
 
-        child: Column(
-          children: [
-            Container(
-              height: 70,
-              color: Colors.deepPurple.withOpacity(0.2),
-              child: Row(
-                children: [
-                  Column(
-                    children: [
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Container(
+                height: 80,
+                color: Colors.deepPurple.withOpacity(0.2),
+                child: Row(
+                  children: [
+                    Column(
+                      children: [
 
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
 
-                          child: CircleAvatar(
-                            backgroundColor: Colors.grey,
-                            radius: 25,
-                            backgroundImage: AssetImage(
-                                "assets/images/event.png"
+                            child: CircleAvatar(
+                              backgroundColor: Colors.grey,
+                              radius: 30,
+                              backgroundImage: AssetImage(
+                                  "assets/images/event.png"
+                              ),
+
                             ),
-
                           ),
                         ),
+                      ],
+                    ),
+
+Column(
+  children: [
+    Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Align(
+          alignment: Alignment.topLeft,
+          child: Row(
+            children: [
+              Text(" Name of the person inbox",
+                style: TextStyle(
+                    //fontWeight: FontWeight.bold
+                  fontSize: 15
+                ),
+              )
+            ],
+          ),
+      ),
+    ),
+    Divider(
+      height: 2,
+    ),
+    Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Align(
+       // alignment: Alignment.center,
+          child: Row(
+            children: [
+              Text("Lorem ipsumLorem ipsum",
+                style: TextStyle(
+                    //fontWeight: FontWeight.bold
+                  color: Colors.grey,
+                ),
+              )
+            ],
+          ),
+      ),
+    ),
+    Row(
+      children: [
+          Text("Lorem ipsumLorem ipsum",
+            style: TextStyle(
+              //fontWeight: FontWeight.bold
+              color: Colors.grey
+            ),
+          )
+      ],
+    ),
+
+  ],
+),
+
+
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(60, 10, 0, 0),
+                      child: Align(
+                        alignment: Alignment.topRight,
+                        child: Column(
+                          children: [
+                            Align(
+                              alignment: Alignment.topRight,
+                              child: Row(
+                                children: [
+                                  Text("16 Mar")
+                                ],
+                              ),
+                            ),
+                            Divider(
+                              height: 15,
+                            ),
+                            Align(
+                              alignment: Alignment.centerLeft,
+                              child: Container(
+                                height: 10,
+                                child: CircleAvatar(
+                                  radius: 10,
+                                  backgroundColor: Colors.yellow,
+
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ],
-                  ),
+                    )
 
 
+                  ],
+                ),
 
 
+              ),
+              Container(
+                height: 80,
+                color: Colors.deepPurple.withOpacity(0.2),
+                child: Row(
+                  children: [
+                    Column(
+                      children: [
+
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+
+                            child: CircleAvatar(
+                              backgroundColor: Colors.grey,
+                              radius: 30,
+                              backgroundImage: AssetImage(
+                                  "assets/images/event.png"
+                              ),
+
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Align(
+                            alignment: Alignment.topLeft,
+                            child: Row(
+                              children: [
+                                Text(" Name of the person inbox",
+                                  style: TextStyle(
+                                    //fontWeight: FontWeight.bold
+                                      fontSize: 15
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                        Divider(
+                          height: 2,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Align(
+                            // alignment: Alignment.center,
+                            child: Row(
+                              children: [
+                                Text("Lorem ipsumLorem ipsum",
+                                  style: TextStyle(
+                                    //fontWeight: FontWeight.bold
+                                    color: Colors.grey,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Text("Lorem ipsumLorem ipsum",
+                              style: TextStyle(
+                                //fontWeight: FontWeight.bold
+                                  color: Colors.grey
+                              ),
+                            )
+                          ],
+                        ),
+
+                      ],
+                    ),
 
 
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(60, 10, 0, 0),
+                      child: Align(
+                        alignment: Alignment.topRight,
+                        child: Column(
+                          children: [
+                            Align(
+                              alignment: Alignment.topRight,
+                              child: Row(
+                                children: [
+                                  Text("16 Mar")
+                                ],
+                              ),
+                            ),
+                            Divider(
+                              height: 15,
+                            ),
+                            Align(
+                              alignment: Alignment.centerLeft,
+                              child: Container(
+                                height: 10,
+                                child: CircleAvatar(
+                                  radius: 10,
+                                  backgroundColor: Colors.yellow,
+
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
 
 
+                  ],
+                ),
 
 
+              ),
+
+              Container(
+                height: 80,
+                color: Colors.deepPurple.withOpacity(0.2),
+                child: Row(
+                  children: [
+                    Column(
+                      children: [
+
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+
+                            child: CircleAvatar(
+                              backgroundColor: Colors.grey,
+                              radius: 30,
+                              backgroundImage: AssetImage(
+                                  "assets/images/event.png"
+                              ),
+
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Align(
+                            alignment: Alignment.topLeft,
+                            child: Row(
+                              children: [
+                                Text(" Name of the person inbox",
+                                  style: TextStyle(
+                                    //fontWeight: FontWeight.bold
+                                      fontSize: 15
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                        Divider(
+                          height: 2,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Align(
+                            // alignment: Alignment.center,
+                            child: Row(
+                              children: [
+                                Text("Lorem ipsumLorem ipsum",
+                                  style: TextStyle(
+                                    //fontWeight: FontWeight.bold
+                                    color: Colors.grey,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Text("Lorem ipsumLorem ipsum",
+                              style: TextStyle(
+                                //fontWeight: FontWeight.bold
+                                  color: Colors.grey
+                              ),
+                            )
+                          ],
+                        ),
+
+                      ],
+                    ),
 
 
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(60, 10, 0, 0),
+                      child: Align(
+                        alignment: Alignment.topRight,
+                        child: Column(
+                          children: [
+                            Align(
+                              alignment: Alignment.topRight,
+                              child: Row(
+                                children: [
+                                  Text("16 Mar")
+                                ],
+                              ),
+                            ),
+                            Divider(
+                              height: 15,
+                            ),
+                            Align(
+                              alignment: Alignment.centerLeft,
+                              child: Container(
+                                height: 10,
+                                child: CircleAvatar(
+                                  radius: 10,
+                                  backgroundColor: Colors.yellow,
+
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
 
 
-                ],
+                  ],
+                ),
+
+
+              ),
+              Container(
+                height: 80,
+                color: Colors.deepPurple.withOpacity(0.2),
+                child: Row(
+                  children: [
+                    Column(
+                      children: [
+
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+
+                            child: CircleAvatar(
+                              backgroundColor: Colors.grey,
+                              radius: 30,
+                              backgroundImage: AssetImage(
+                                  "assets/images/event.png"
+                              ),
+
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Align(
+                            alignment: Alignment.topLeft,
+                            child: Row(
+                              children: [
+                                Text(" Name of the person inbox",
+                                  style: TextStyle(
+                                    //fontWeight: FontWeight.bold
+                                      fontSize: 15
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                        Divider(
+                          height: 2,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Align(
+                            // alignment: Alignment.center,
+                            child: Row(
+                              children: [
+                                Text("Lorem ipsumLorem ipsum",
+                                  style: TextStyle(
+                                    //fontWeight: FontWeight.bold
+                                    color: Colors.grey,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Text("Lorem ipsumLorem ipsum",
+                              style: TextStyle(
+                                //fontWeight: FontWeight.bold
+                                  color: Colors.grey
+                              ),
+                            )
+                          ],
+                        ),
+
+                      ],
+                    ),
+
+
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(60, 10, 0, 0),
+                      child: Align(
+                        alignment: Alignment.topRight,
+                        child: Column(
+                          children: [
+                            Align(
+                              alignment: Alignment.topRight,
+                              child: Row(
+                                children: [
+                                  Text("16 Mar")
+                                ],
+                              ),
+                            ),
+                            Divider(
+                              height: 15,
+                            ),
+                            Align(
+                              alignment: Alignment.centerLeft,
+                              child: Container(
+                                height: 10,
+                                child: CircleAvatar(
+                                  radius: 10,
+                                  backgroundColor: Colors.yellow,
+
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+
+
+                  ],
+                ),
+
+
               ),
 
 
-            ),
-          ],
+
+
+
+            ],
+          ),
         ),
       ),
     );
